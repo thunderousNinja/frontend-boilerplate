771b06344adad1a4c1d4b1ecda4ba109538a289e
Jonathan Garcia <jon4thangarcia@gmail.com>
2016-10-19 01:12:33 -0700

    Add react-css-modules

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import CSSModules from 'react-css-modules';
 import styles from './index.scss';
 
+@CSSModules(styles)
 class App extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.any
+  }
+
   render() {
+    
     return (
-      <div className={ styles.app }>
+      <div styleName='app'>
         { this.props.children }
       </div>
       );
   }
-}
-
-App.propTypes = {
-  children: React.PropTypes.object
 }
 
 export default App;
